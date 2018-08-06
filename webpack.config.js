c8b5a949a3495101694da14b7089dd2a5534e070
mugren <igorkivor@gmail.com>
2018-08-06 23:46:44 +0200

    update other

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,17 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       exclude: /node_modules/,
-  //       use: ['babel-loader']
-  //     }
-  //   ]
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
   devServer: {
     port: 3000,
     contentBase: path.resolve(__dirname, 'build')
   }
-}
+};
