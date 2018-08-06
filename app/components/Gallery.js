a5eaaaeafa0c4ebcd916e550824795aee0b1fb78
mugren <igorkivor@gmail.com>
2018-08-06 23:43:59 +0200

    demo app

---
+++ app/components/Gallery.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+ class Gallery extends Component {
+   render(){
+     let alternate =  'http://www.favful.com/images/original/missing.png';
+
+     return (
+       <div>
+         {
+           this.props.items.map((item, index) => {
+             let {title, imageLinks, infoLink} = item.volumeInfo;
+             return (
+               <a
+                  key={index}
+                  className="book"
+                  href={infoLink}
+                  target="_blank"
+                >
+                  <img
+                    src={ imageLinks !== undefined ? imageLinks.thumbnail : alternate }
+                    alt="book image"
+                    className="book-img"
+                  />
+                  <div className="book-text">
+                     {title}
+                  </div>
+               </a>
+             )
+           })
+         }
+       </div>
+     )
+   }
+ }
+
+ export default Gallery;
---
+++ app/components/global.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class Global extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        query: ''
+      }
+  }
+
+  search() {
+    console.log(this.state.query);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Book Explorer!</h2>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for a book"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+      </div>
+    )
+  }
+}
+
+export default Global;
